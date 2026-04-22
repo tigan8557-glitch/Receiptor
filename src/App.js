e1e0e0b5466c9fba7fc0341f8eec08843074e809
tigan8557-glitch <tigan8557@gmail.com>
2026-04-23 01:15:54 +0300

    Implement routing for ReceiptForm and ReceiptPage

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-App.js
-
 import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { ThemeProvider } from './context/ThemeContext';
 import { ReceiptForm } from './components/ReceiptForm';
+import ReceiptPage from './pages/ReceiptPage';
 import './App.css';
 
 function App() {
   return (
     <ThemeProvider>
       <div className="App">
-        <ReceiptForm />
+        <Routes>
+          <Route path="/" element={<ReceiptForm />} />
+          <Route path="/receipt" element={<ReceiptPage />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
       </div>
     </ThemeProvider>
   );
