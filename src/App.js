2e5911bd8c698db746a1ad0157a32b0d46c70bdf
tigan8557-glitch <tigan8557@gmail.com>
2026-04-23 01:06:20 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
+App.js
+
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { ThemeProvider } from './context/ThemeContext';
 import { ReceiptForm } from './components/ReceiptForm';
-import ReceiptPage from './pages/ReceiptPage';
 import './App.css';
 
 function App() {
   return (
     <ThemeProvider>
-      <Router>
-        <div className="App">
-          <Routes>
-            <Route path="/" element={<ReceiptForm />} />
-            <Route path="/receipt" element={<ReceiptPage />} />
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Routes>
-        </div>
-      </Router>
+      <div className="App">
+        <ReceiptForm />
+      </div>
     </ThemeProvider>
   );
 }
